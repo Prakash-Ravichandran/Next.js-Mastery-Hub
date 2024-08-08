50a5280037b656686ee14c6a7a4274de829d69f4
Prakash Ravichandran <prakashmtech1998@gmail.com>
2024-08-08 23:17:44 +0530

    [refactor]: [95] add app/

--- app/page.js
+++ app/page.js
@@ -1,15 +1,9 @@
-import Header from "@/components/header";
-
 export default function Home() {
-
-  console.log("* * * Executing the Home Component * * *");
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <a href="/about">About us</a>
-      <Header />
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }

ca40ee27ee700ed458626c93ba90ae526836e4de
Prakash Ravichandran <prakashmtech1998@gmail.com>
2024-08-11 13:39:30 +0530

    [feat]: add nav item

--- components/main-header.js
+++ components/main-header.js
@@ -4,12 +4,22 @@ import logoImg from "@/assets/logo.png";
 export default function MainHeader(){
     return (
     <>
-      <header>
-        <Link href='/'>
-             <img src={logoImg.src} alt="A Plate with food on it"/>
-             Next level Food
-        </Link>
-      </header>
+        <header>
+            <Link href='/'>
+            <img src={logoImg.src} alt="A Plate with food on it"/>
+            Next level Food
+            </Link>
+        </header>
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/meals">Browser meals</Link>
+                </li>
+                <li>
+                    <Link href="/community">Food Community</Link>
+                </li>
+            </ul>
+        </nav>
     </>
     );
 }

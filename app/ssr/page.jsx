216cff7f789545c58990a322bffa6b97bf030c55
Prakash Ravichandran <prakashmtech1998@gmail.com>
2024-08-12 19:56:19 +0530

    [feat]: integrate SSR

--- app/ssr/page.jsx
+++ app/ssr/page.jsx
@@ -1,5 +1,30 @@
-export default function SSRComponent(){
-    return (<>
-       <h2>This is an SSR Route example</h2>
-    </>);
+const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const data = await res.json();
+    console.log(`fetching data =` + data);
+  
+    return data;
+  };
+  
+
+
+export default async function SSRComponent() {
+  const  todos = await getData();
+
+  return (
+    <>
+      {todos?.length == 0 ? (
+        <p className="route"> Loading ...</p>
+      ) : (
+        <p className="route"> *** Page is rendered with SSR data ***</p>
+      )}
+      {todos?.map((item) => (
+        <p key={item.key} className={"route"}>
+          Todo item {item.id}. - {item.title}
+        </p>
+      ))}
+    </>
+  );
 }
+
+

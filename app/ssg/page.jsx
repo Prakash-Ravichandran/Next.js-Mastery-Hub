f14d619c774b8517ac6c46e6ed3a0035cfc40c30
Prakash Ravichandran <prakashmtech1998@gmail.com>
2024-08-12 08:40:29 +0530

    rendering types route setup -

---
+++ app/ssg/page.jsx
@@ -0,0 +1,5 @@
+export default function SSGComponent(){
+    return (<>
+       <h2>This is an SSG route example</h2>
+    </>);
+}
---
+++ app/ssr/page.jsx
@@ -0,0 +1,5 @@
+export default function SSRComponent(){
+    return (<>
+       <h2>This is an SSR Route example</h2>
+    </>);
+}

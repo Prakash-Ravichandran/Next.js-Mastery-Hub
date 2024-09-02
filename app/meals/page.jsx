01c3a3e6d364547ed7e705be1ab169c20ba632bc
Prakash Ravichandran <prakashmtech1998@gmail.com>
2024-09-02 19:56:28 +0530

    [feat]: add Suspense Loading for dynamic portion alone

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,15 +2,25 @@ import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import Link from "next/link";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import Loading from "./loading-out";
 
-export default async function Meals() {
+async function Meals() {
   const meals = await getMeals();
 
   return (
     <>
+      <MealsGrid meals={meals} />
+    </>
+  );
+}
+
+export default async function MealsPage() {
+  return (
+    <>
       <header className={classes.header}>
         <h1>
-          Delicious meals, created{" "}
+          Delicious meals, created
           <span className={classes.highlight}>by you</span>
         </h1>
         <p>
@@ -21,7 +31,9 @@ export default async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<Loading/>}>
+           <Meals />
+        </Suspense>
       </main>
     </>
   );

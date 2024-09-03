c021997822d4a830e5aea4ee3b235cb3afd9be73
Prakash Ravichandran <prakashmtech1998@gmail.com>
2024-09-03 12:57:57 +0530

    [feat]: add individual meal

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,5 +1,32 @@
-export default function DynamicRouteComponent({params}){
-    return(<>
-       <p>{`This page is a dynamic route - ${params.slug}`}</p>
-    </>);
+import Image from "next/image";
+import Link from "next/link";
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
+export default function mealDetailPage({ params }) {
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h2>{meal.title}</h2>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
